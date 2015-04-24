ab56e47c8679a5a0cdfea17c4fc683b0d97f1d04
Joren <jorenvandamme@hotmail.com>
2015-04-24 15:57:12 +0200

    Labo 8 oef 2: aangemaakt

---
+++ Labo/Week_8/Oefening2/KotinKortrijk/app/src/main/java/be/howest/nmct/studentenhuizenkortrijk/StudentenhuizenActivity.java
@@ -0,0 +1,39 @@
+package be.howest.nmct.studentenhuizenkortrijk;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class StudentenhuizenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studentenhuizen);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_studentenhuizen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
